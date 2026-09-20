64dd563d3750c644e95159bc9ba2e6b309d5f09f
Kira EB <107140669+KemmettBuck@users.noreply.github.com>
2026-09-20 18:06:31 -0400

    follow along

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,17 +1,15 @@
 function ListGroup() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
 
+  const handleClick = (event) => console.log(event);
+
   return (
     <>
       <h1>List</h1>
       {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li
-            className="list-group-item"
-            key={item}
-            onClick={() => console.log("Clicked")}
-          >
+        {items.map((item, index) => (
+          <li className="list-group-item" key={item} onClick={}>
             {item}
           </li>
         ))}
