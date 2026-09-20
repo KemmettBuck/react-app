8db32c1548db805f0ecddd58bea62bd7637f6173
Kira EB <107140669+KemmettBuck@users.noreply.github.com>
2026-09-20 19:26:28 -0400

    add onSelectItem

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -3,6 +3,8 @@ import { useState } from "react";
 interface Props {
   items: string[];
   heading: string;
+  // (item: string) => void
+  onSelectItem: (item: string) => void;
 }
 
 function ListGroup({ items, heading }: Props) {
