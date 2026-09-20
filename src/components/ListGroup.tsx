70c730433ebc8c2e5b3efb74d1d1f2fdd40a1c47
Kira EB <107140669+KemmettBuck@users.noreply.github.com>
2026-09-20 17:13:00 -0400

    add brackets for the Prettier format to reformat ListGroups tsx file

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,17 +1,13 @@
 function ListGroup() {
-    const items = [
-        'New York',
-        'San Francisco',
-        'Tokyo',
-        'London',
-        'Paris'
-    ];
+  const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
 
   return (
     <>
       <h1>List</h1>
       <ul className="list-group">
-        items.map(item => <li>{item}</li>)
+        {items.map((item) => (
+          <li>{item}</li>
+        ))}
       </ul>
     </>
   );
