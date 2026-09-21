59a51804378b57b13490eb81955b69c09ee5649a
Kira EB <107140669+KemmettBuck@users.noreply.github.com>
2026-09-20 21:41:34 -0400

    add color back to button in app tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import Button from "./components/Button";
 function App() {
   return (
     <div>
-      <Button onClick={() => console.log("Clicked")}>Kira's Button</Button>
+      <Button color="danger" onClick={() => console.log("Clicked")}>
+        Kira's Button
+      </Button>
     </div>
   );
 }
