8c6e25319102724bf05a2ccfebf96352446e5ddf
Kira EB <107140669+KemmettBuck@users.noreply.github.com>
2026-09-20 21:38:24 -0400

    change button color

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Button from "./components/Button";
 function App() {
   return (
     <div>
-      <Button>Kira's Button</Button>
+      <Button onClick={() => console.log("Clicked")}>Kira's Button</Button>
     </div>
   );
 }
